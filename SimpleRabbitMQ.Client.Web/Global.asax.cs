81cde5ff60b5092ec9f370af9fa9f4d0d771fec7
Michael McCarthy <mike_mccarthy3@yahoo.com>
2019-11-21 12:08:07 -0500

    Remove NServiceBus.NHiberntate and NHiberate packages from .Client.Web

--- SimpleRabbitMQ.Client.Web/Global.asax.cs
+++ SimpleRabbitMQ.Client.Web/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using NServiceBus;
-using NServiceBus.Persistence;
 using SimpleRabbitMQ.Messages;
 
 namespace SimpleRabbitMQ.Client.Web
