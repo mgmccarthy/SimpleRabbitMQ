f129beb804e12865145b69bb6b2a0293933d04bb
Michael McCarthy <mike_mccarthy@yahoo.com>
2019-11-22 16:46:00 -0500

    do not sync this commit to master

--- SimpleRabbitMQ.Endpoint1/Program.cs
+++ SimpleRabbitMQ.Endpoint1/Program.cs
@@ -32,15 +32,16 @@ namespace SimpleRabbitMQ.Endpoint1
             endpointConfiguration.EnableInstallers();
             //endpointConfiguration.EnableOutbox();
 
+            //var persistence = endpointConfiguration.UsePersistence<InMemoryPersistence>();
             var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>();
             persistence.ConnectionString(@"Data Source=(LocalDB)\MSSQLLocalDB; Initial Catalog=NServiceBusNHibernatePersistence; Integrated Security=True;");
 
-            //https://docs.particular.net/monitoring/metrics/
-            //https://docs.particular.net/monitoring/metrics/install-plugin
-            var metrics = endpointConfiguration.EnableMetrics();
-            endpointConfiguration.UniquelyIdentifyRunningInstance()
-                .UsingNames(instanceName: endpointName, hostName: Environment.MachineName);
-            metrics.SendMetricDataToServiceControl(serviceControlMetricsAddress: "Particular.Rabbitmq.Monitoring", interval: TimeSpan.FromSeconds(10));
+            ////https://docs.particular.net/monitoring/metrics/
+            ////https://docs.particular.net/monitoring/metrics/install-plugin
+            //var metrics = endpointConfiguration.EnableMetrics();
+            //endpointConfiguration.UniquelyIdentifyRunningInstance()
+            //    .UsingNames(instanceName: endpointName, hostName: Environment.MachineName);
+            //metrics.SendMetricDataToServiceControl(serviceControlMetricsAddress: "Particular.Rabbitmq.Monitoring", interval: TimeSpan.FromSeconds(10));
 
             //this did nothing to help throughput
             //var timeoutManager = endpointConfiguration.TimeoutManager();
