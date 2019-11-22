c0f07f36eb923764116e4078652b2ce01fe218ab
Michael McCarthy <mike_mccarthy3@yahoo.com>
2019-11-21 21:31:33 -0500

    Got Client.Web running in self-hosting mode as a send-only endpoint.
    - had to add NServiceBus.RabbitMQ to the project, and it pretty much worked as is

--- SimpleRabbitMQ.Client.Web/Global.asax.cs
+++ SimpleRabbitMQ.Client.Web/Global.asax.cs
@@ -12,39 +12,38 @@ namespace SimpleRabbitMQ.Client.Web
     {
         public static IEndpointInstance Endpoint;
 
-        //protected void Application_Start()
-        //{
-        //    var endpointConfiguration = new EndpointConfiguration("SimpleRabbitMQ.Client.Web");
-        //    endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
-        //    endpointConfiguration.SendFailedMessagesTo("SimpleRabbitMQ.Error");
-        //    endpointConfiguration.AuditProcessedMessagesTo("SimpleRabbitMQ.Audit");
-        //    var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
-        //    transport.Routing().RouteToEndpoint(typeof(TestCommand), "SimpleRabbitMQ.Endpoint1");
-        //    transport.UseConventionalRoutingTopology();
-        //    transport.ConnectionString("host=localhost");
-        //    endpointConfiguration.EnableInstallers();
-        //    //endpointConfiguration.EnableOutbox();
-            
-        //    //getting rid of SendOnly, aka, putting a queue under Client.Web seems to have a 1:1 dependency on each http request that is processed by the API controller
-        //    //to be subsequently tied to the processing of that message in the endpoint. With SendOnly, the messages throughput was majorly quicker and the endpoint had to catch
-        //    //up to all the messages put onto the endpoint queue
-        //    endpointConfiguration.SendOnly();
+        protected void Application_Start()
+        {
+            var endpointConfiguration = new EndpointConfiguration("SimpleRabbitMQ.Client.Web");
+            endpointConfiguration.SendFailedMessagesTo("SimpleRabbitMQ.Error");
+            endpointConfiguration.AuditProcessedMessagesTo("SimpleRabbitMQ.Audit");
+            var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+            transport.Routing().RouteToEndpoint(typeof(TestCommand), "SimpleRabbitMQ.Endpoint1");
+            //transport.UseConventionalRoutingTopology();
+            transport.ConnectionString("host=localhost");
+            endpointConfiguration.EnableInstallers();
+            //endpointConfiguration.EnableOutbox();
 
-        //    ////if this is not a SendOnly endpoint, then we need to set the persistence to something that supports outbox
-        //    //var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>();
-        //    //persistence.ConnectionString(@"Data Source=(LocalDB)\MSSQLLocalDB; Initial Catalog=NServiceBusNHibernatePersistence; Integrated Security=True;");
+            //getting rid of SendOnly, aka, putting a queue under Client.Web seems to have a 1:1 dependency on each http request that is processed by the API controller
+            //to be subsequently tied to the processing of that message in the endpoint. With SendOnly, the messages throughput was majorly quicker and the endpoint had to catch
+            //up to all the messages put onto the endpoint queue
+            endpointConfiguration.SendOnly();
 
-        //    Endpoint = NServiceBus.Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
-        //    AreaRegistration.RegisterAllAreas();
-        //    GlobalConfiguration.Configure(WebApiConfig.Register);
-        //    FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-        //    RouteConfig.RegisterRoutes(RouteTable.Routes);
-        //    BundleConfig.RegisterBundles(BundleTable.Bundles);
-        //}
+            ////if this is not a SendOnly endpoint, then we need to set the persistence to something that supports outbox
+            //var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>();
+            //persistence.ConnectionString(@"Data Source=(LocalDB)\MSSQLLocalDB; Initial Catalog=NServiceBusNHibernatePersistence; Integrated Security=True;");
 
-        //protected void Application_End()
-        //{
-        //    Endpoint?.Stop().GetAwaiter().GetResult();
-        //}
+            Endpoint = NServiceBus.Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_End()
+        {
+            Endpoint?.Stop().GetAwaiter().GetResult();
+        }
     }
 }
