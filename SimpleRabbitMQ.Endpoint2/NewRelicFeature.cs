fc68e23d5656a81c0d078f162ff5832f14921ef7
Michael McCarthy <mike_mccarthy@yahoo.com>
2020-01-08 08:09:24 -0500

    Disable NewRelic Features

--- SimpleRabbitMQ.Endpoint2/NewRelicFeature.cs
+++ SimpleRabbitMQ.Endpoint2/NewRelicFeature.cs
@@ -6,77 +6,77 @@ using static NewRelic.Api.Agent.NewRelic;
 
 namespace SimpleRabbitMQ.Endpoint2
 {
-    public class NewRelicFeature : Feature
+    public class NewRelicFeature //: Feature
     {
-        public NewRelicFeature()
-        {
-            Defaults(settings =>
-            {
-                metricsOptions = settings.EnableMetrics();
-            });
-            EnableByDefault();
-        }
+    //    public NewRelicFeature()
+    //    {
+    //        Defaults(settings =>
+    //        {
+    //            metricsOptions = settings.EnableMetrics();
+    //        });
+    //        EnableByDefault();
+    //    }
 
-        protected override void Setup(FeatureConfigurationContext context)
-        {
-            var settings = context.Settings;
+    //    protected override void Setup(FeatureConfigurationContext context)
+    //    {
+    //        var settings = context.Settings;
 
-            var endpointName = settings.EndpointName();
-            var nameMapping = new Dictionary<string, string>
-            {
-                // https://docs.newrelic.com/docs/agents/manage-apm-agents/agent-data/collect-custom-metrics
-                {"# of msgs successfully processed / sec", FormatMetric("Success_Total", endpointName)},
-                {"# of msgs pulled from the input queue /sec", FormatMetric("Fetched_Total", endpointName)},
-                {"# of msgs failures / sec", FormatMetric("Failure_Total", endpointName)},
-                {"Critical Time", FormatMetric("CriticalTime_Seconds", endpointName)},
-                {"Processing Time", FormatMetric("ProcessingTime_Seconds", endpointName)},
-            };
+    //        var endpointName = settings.EndpointName();
+    //        var nameMapping = new Dictionary<string, string>
+    //        {
+    //            // https://docs.newrelic.com/docs/agents/manage-apm-agents/agent-data/collect-custom-metrics
+    //            {"# of msgs successfully processed / sec", FormatMetric("Success_Total", endpointName)},
+    //            {"# of msgs pulled from the input queue /sec", FormatMetric("Fetched_Total", endpointName)},
+    //            {"# of msgs failures / sec", FormatMetric("Failure_Total", endpointName)},
+    //            {"Critical Time", FormatMetric("CriticalTime_Seconds", endpointName)},
+    //            {"Processing Time", FormatMetric("ProcessingTime_Seconds", endpointName)},
+    //        };
 
-            metricsOptions.RegisterObservers(
-            register: probeContext =>
-            {
-                RegisterProbes(probeContext, endpointName, nameMapping);
-            });
-        }
+    //        metricsOptions.RegisterObservers(
+    //        register: probeContext =>
+    //        {
+    //            RegisterProbes(probeContext, endpointName, nameMapping);
+    //        });
+    //    }
 
-        public void RegisterProbes(ProbeContext context, string endpointName, Dictionary<string, string> nameMapping)
-        {
-            foreach (var duration in context.Durations)
-            {
-                duration.Register((ref DurationEvent @event) =>
-                {
-                    nameMapping.TryGetValue(duration.Name, out var mappedName);
-                    var newRelicName = string.Format(mappedName ?? FormatMetric(duration.Name, endpointName), Normalize(@event.MessageType));
-                    RecordResponseTimeMetric(newRelicName, Convert.ToInt64(@event.Duration.TotalMilliseconds));
-                });
-            }
-            foreach (var signal in context.Signals)
-            {
-                signal.Register((ref SignalEvent @event) =>
-                {
-                    nameMapping.TryGetValue(signal.Name, out var mappedName);
-                    var newRelicName = string.Format(mappedName ?? FormatMetric(signal.Name, endpointName), Normalize(@event.MessageType));
-                    RecordMetric(newRelicName, 1);
-                });
-            }
-        }
+    //    public void RegisterProbes(ProbeContext context, string endpointName, Dictionary<string, string> nameMapping)
+    //    {
+    //        foreach (var duration in context.Durations)
+    //        {
+    //            duration.Register((ref DurationEvent @event) =>
+    //            {
+    //                nameMapping.TryGetValue(duration.Name, out var mappedName);
+    //                var newRelicName = string.Format(mappedName ?? FormatMetric(duration.Name, endpointName), Normalize(@event.MessageType));
+    //                RecordResponseTimeMetric(newRelicName, Convert.ToInt64(@event.Duration.TotalMilliseconds));
+    //            });
+    //        }
+    //        foreach (var signal in context.Signals)
+    //        {
+    //            signal.Register((ref SignalEvent @event) =>
+    //            {
+    //                nameMapping.TryGetValue(signal.Name, out var mappedName);
+    //                var newRelicName = string.Format(mappedName ?? FormatMetric(signal.Name, endpointName), Normalize(@event.MessageType));
+    //                RecordMetric(newRelicName, 1);
+    //            });
+    //        }
+    //    }
 
-        static string Normalize(string name)
-        {
-            var result = name.Split(SplitComma, StringSplitOptions.RemoveEmptyEntries);
-            if (result.Length > 0)
-            {
-                name = result[0];
-            }
-            return name.Replace(" ", "_").Replace(".", "_").Replace("-", "_");
-        }
+    //    static string Normalize(string name)
+    //    {
+    //        var result = name.Split(SplitComma, StringSplitOptions.RemoveEmptyEntries);
+    //        if (result.Length > 0)
+    //        {
+    //            name = result[0];
+    //        }
+    //        return name.Replace(" ", "_").Replace(".", "_").Replace("-", "_");
+    //    }
 
-        static string FormatMetric(string name, string prefix)
-        {
-            return Normalize($"Custom/NServiceBus/{prefix}/{{0}}/{name}");
-        }
+    //    static string FormatMetric(string name, string prefix)
+    //    {
+    //        return Normalize($"Custom/NServiceBus/{prefix}/{{0}}/{name}");
+    //    }
 
-        static string[] SplitComma = new[] { "," };
-        MetricsOptions metricsOptions;
-    }
+    //    static string[] SplitComma = new[] { "," };
+    //    MetricsOptions metricsOptions;
+    //}
 }
